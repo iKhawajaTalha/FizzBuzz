0a4928e59fe4d09317ed01162fb7b240f387550f
Khawaja Talha Humayun <kh.talha.humayun@gmail.com>
2024-09-19 15:43:19 +0500

    Update fizzBuzz.js
    added while loop

--- fizzBuzz.js
+++ fizzBuzz.js
@@ -3,19 +3,20 @@ var count = 1;
 
 function fizzBuzz() {
 
-    if (count % 3 === 0 && count % 5 === 0) {
-        output.push("FizzBuzz"); //This will print FizzBuzz instead of multiples of 3 & 5
-    }
-    else if (count % 3 === 0) {
-        output.push("Fizz"); //This will print Fizz instead of multiples of 3
-    } else if (count % 5 === 0) {
-        output.push("Buzz"); //This will print Buzz instead of multiples of 5
-    }
-    else {
-        output.push(count);
-    }
+    while (count <= 100) {
 
-    count++;
-
+        if (count % 3 === 0 && count % 5 === 0) {
+            output.push("FizzBuzz"); //This will print FizzBuzz instead of multiples of 3 & 5
+        }
+        else if (count % 3 === 0) {
+            output.push("Fizz"); //This will print Fizz instead of multiples of 3
+        } else if (count % 5 === 0) {
+            output.push("Buzz"); //This will print Buzz instead of multiples of 5
+        }
+        else {
+            output.push(count);
+        }
+        count++;
+    }
     console.log(output);
 }
