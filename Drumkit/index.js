0f044475c8d49df71eb91949adb8b16d13c9ca52
Khawaja Talha Humayun <kh.talha.humayun@gmail.com>
2024-09-30 12:40:06 +0500

    Drumkit is done

---
+++ Drumkit/index.js
@@ -0,0 +1,11 @@
+
+var numOfDrumButtons = document.querySelectorAll(".drum").length; 
+for (var i = 0; i<numOfDrumButtons; i++){
+
+    document.querySelectorAll(".drum")[i].addEventListener("click", function(){
+        alert("I'm Clicked!");
+    });
+}
+
+
+
