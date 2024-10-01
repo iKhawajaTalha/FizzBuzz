9f0ef6e4f203bb814ef4ffe8c7d3f45d4a0897e4
Khawaja Talha Humayun <kh.talha.humayun@gmail.com>
2024-10-01 12:35:35 +0500

    added sounds on click

--- Drumkit/index.js
+++ Drumkit/index.js
@@ -1,11 +1,48 @@
 // Drum Buttons
-var numOfDrumButtons = document.querySelectorAll(".drum").length; 
-for (var i = 0; i<numOfDrumButtons; i++){
+var numOfDrumButtons = document.querySelectorAll(".drum").length;
+for (var i = 0; i < numOfDrumButtons; i++) {
+  document.querySelectorAll(".drum")[i].addEventListener("click", function () {
+    var buttonInnerHTML = this.innerHTML;
 
-    document.querySelectorAll(".drum")[i].addEventListener("click", function(){
-        alert("I'm Clicked!");
-    });
+    //Audio added to buttons with switch statment
+    switch (buttonInnerHTML) {
+
+      case "w":
+        var audio = new Audio("sounds/crash.mp3");
+        audio.play();
+        break;
+
+      case "a":
+        var audio = new Audio("sounds/kick-bass.mp3");
+        audio.play();
+        break;
+
+      case "s":
+        var audio = new Audio("sounds/snare.mp3");
+        audio.play();
+        break;
+
+      case "d":
+        var audio = new Audio("sounds/tom-1.mp3");
+        audio.play();
+        break;
+
+      case "j":
+        var audio = new Audio("sounds/tom-2.mp3");
+        audio.play();
+        break;
+
+      case "k":
+        var audio = new Audio("sounds/tom-3.mp3");
+        audio.play();
+        break;
+
+      case "l":
+        var audio = new Audio("sounds/tom-4.mp3");
+        audio.play();
+        break;
+
+      default: console.log(buttonInnerHTML);
+    }
+  });
 }
-
-
-
